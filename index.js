dc69911ab416869ec281e3995ce0b2c0f305e953
SebasNatale <giordano_95@hotmail.com>
2020-06-09 14:40:47 -0500

    xd

--- index.js
+++ index.js
@@ -7,17 +7,23 @@ const sequelize = require("sequelize");
 const jwt = require("jsonwebtoken");
 const key = require("./shh");
 
-const sql = new sequelize("mysql://lJREna0GOO:Vf5yMxDH7x@remotemysql.com:3306/lJREna0GOO");
-sql.authenticate().then(() => console.log("DB conectada!"));
 const server = express();
 server.use(cors());
 server.use(parser.json());
 server.listen(3000, () => console.log("Servidor iniciado!"));
+const sql = new sequelize("mysql://lJREna0GOO:Vf5yMxDH7x@remotemysql.com:3306/lJREna0GOO");
+sql.authenticate().then(() => console.log("DB conectada!"));
 
 //--------------------------------------------------------- ENDPOINTS
+//----------------------- PRODUCTOS
 
-//PRODUCTOS
-server.get("/productos", async function (req, res) {
-    var [xd] = await sql.query("SELECT * FROM productos", {raw: true})
-    res.json(xd)
+//Obtener lista
+server.get("/productos", async function(req, res) {
+    var [list] = await sql.query("SELECT * FROM productos", {raw: true})
+    res.json(list)
 })
+
+//Crear items
+server.post("/products", async function(req, res) {
+    
+});
