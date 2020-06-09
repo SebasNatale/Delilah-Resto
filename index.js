689d7630de4eaa84d731195c7fc9f6cd1dd7cee6
SebasNatale <giordano_95@hotmail.com>
2020-06-09 11:09:04 -0500

    lol

---
+++ index.js
@@ -0,0 +1,19 @@
+//--------------------------------------------------------- DEPENDENCIAS 'N STUFF
+
+const express = require("express");
+const parser = require("body-parser");
+const cors = require("cors");
+const sequelize = require("sequelize");
+const jwt = require("jsonwebtoken");
+const key = require("./shh");
+
+const sql = new sequelize("mysql://lJREna0GOO:Vf5yMxDH7x@remotemysql.com:3306/lJREna0GOO");
+const server = express();
+server.use(cors());
+server.use(parser.json());
+server.listen(3000, () => {
+    console.log("Servidor iniciado!")
+});
+
+//--------------------------------------------------------- SOME OTHER SHIT
+
