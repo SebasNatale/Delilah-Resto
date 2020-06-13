fe8853a182cea068a6c6b96fb9d6903791e00474
SebasNatale <giordano_95@hotmail.com>
2020-06-13 12:31:00 -0500

    pues aja

--- index.js
+++ index.js
@@ -17,16 +17,17 @@ sql.authenticate().then(() => console.log("DB conectada!"));
 //--------------------------------------------------------- ENDPOINTS
 //----------------------- PRODUCTOS
 
-//Obtener lista
+//Obtener lista de productos
 server.get("/productos", async function(req, res) {
     var [lista] = await sql.query("SELECT * FROM productos")
     res.json(lista)
-})
+});
 
+//Obtener producto por ID
 server.get("/productos/:id", async function(req, res) {
     var [lista] = await sql.query(`SELECT * FROM productos WHERE product_id = "${req.params.id}"`)
     res.json(lista)
-})
+});
 
 //Crear items
 server.post("/productos", async function(req, res) {
@@ -36,4 +37,37 @@ server.post("/productos", async function(req, res) {
         VALUES ("${nombre}", "${precio}", "${link_foto}")`)
     var [id] = await sql.query("SELECT * FROM productos ORDER BY product_id DESC LIMIT 1")
     res.json(id)
+});
+
+//Editar item por ID
+server.put("/productos/:id", async function(req, res) {
+    var {nombre, precio, link_foto} = req.body
+    async function edicion(llave, valor) {
+        await sql.query(`UPDATE productos SET ${llave} = "${valor}" WHERE product_id = ${req.params.id}`)
+    }
+    try {
+        if (nombre) {
+            edicion(Object.getOwnPropertyNames(req.body), nombre)
+            var [actualizado] = await sql.query(`SELECT * FROM productos WHERE product_id = ${req.params.id}`)
+            res.json(actualizado)
+        } else if (precio) {
+            edicion(Object.getOwnPropertyNames(req.body), precio)
+            var [actualizado] = await sql.query(`SELECT * FROM productos WHERE product_id = ${req.params.id}`)
+            res.json(actualizado)
+        } else if (link_foto) {
+            edicion(Object.getOwnPropertyNames(req.body), link_foto)
+            var [actualizado] = await sql.query(`SELECT * FROM productos WHERE product_id = ${req.params.id}`)
+            res.json(actualizado)
+        } else {
+            res.send("No se encontraron datos para actualizar!")
+        }
+    } catch (error) {
+        res.send("Solo se puede actualizar un dato a la vez!")
+    }
+});
+
+//Borrar item por ID
+server.delete("/productos/:id", async function(req, res) {
+    await sql.query(`DELETE FROM productos WHERE product_id = "${req.params.id}"`)
+    res.send("Producto eliminado!")
 });
