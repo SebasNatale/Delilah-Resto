71a641ee175ddbe07e07d1e1eda37fd62c5b01d7
SebasNatale <giordano_95@hotmail.com>
2020-06-22 21:55:16 -0500

    Agregado endpoint edicion pedido

--- index.js
+++ index.js
@@ -198,3 +198,13 @@ server.post("/pedidos", async function(req, res) {
         res.send("Token no encontrado o expirado. Inicie sesion antes de continuar!")
     }
 });
+
+//Actualizar el estado de una orden
+server.put("/pedidos/:id", adminAuth, async function(req, res) {
+    var edit = req.body
+    await sql.query(`
+        UPDATE ordenes SET estado = "${edit.estado}" WHERE order_id = "${req.params.id}"
+    `)
+    var [respo] = await sql.query(`SELECT * FROM ordenes WHERE order_id = "${req.params.id}"`)
+    res.json(respo)
+});
